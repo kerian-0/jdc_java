c191dc4ec81e9c6b0750c4286f566dbf543b89ef
keren <james342506@gmail.com>
2025-03-03 13:28:00 +0630

    hey

--- JDC Class/exception-project/src/test/java/com/jdc/ly/test/HandlerTest.java
+++ JDC Class/exception-project/src/test/java/com/jdc/ly/test/HandlerTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 import com.jdc.ly.handler.Handler;
 
 public class HandlerTest {
-
-
-	@Test
+	
+    @Test
 	void estName() {
 		Handler handle=new Handler();
 		handle.useName();
+		
 	}
 
 }
